d9f8a0b374a19cc70403d04afb0f2cc950103120
Cedric Mensah <cedric@mensah.de>
2019-04-06 17:07:34 +0200

    First Commit

---
+++ screamers.js
@@ -0,0 +1,22 @@
+var mic = 0
+let x = 50
+
+function setup() {
+
+    createCanvas(640,480)
+    ellipse(50, 400, 80, 80)
+    ellipse(560, 400, 80, 80)
+
+
+}
+
+
+function draw () {
+
+        stroke(50)
+        fill(100)
+        ellipse(x, 400, 24,24)
+        x = x+1
+        
+    
+    }
